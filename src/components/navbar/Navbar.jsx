cbc4ea369185f38ed53c668d10048bb7adb2c9f6
Gabriel Ferreira <willamigabrief.souza@gmail.com>
2023-07-31 18:12:58 -0300

    New navbar update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,34 @@
+import { useState } from 'react';
+
 import {
     NavbarStyled,
     MenuToggle
 } from "./NavbarStyled"
 
 export default function Navbar(){
+    const [show, setShow] = useState(false);
+
+    const toggleMenu = () => {
+        setShow(!show);
+    }
+
     return(
-        <NavbarStyled>
-            <MenuToggle>
+        <NavbarStyled open={show}>
+            <MenuToggle onClick={toggleMenu}>
                 <div className="stick01"></div>
                 <div className="stick02"></div>
                 <div className="stick03"></div>
             </MenuToggle>
+            <nav>
+                <ul> 
+                    <li>Home</li>
+                    <li>New</li>
+                    <li>Popular</li>
+                    <li>Treading</li>
+                    <li>Categories</li>
+                </ul>
+            </nav>
+
         </NavbarStyled>
     )
 }
--- src/components/navbar/NavbarStyled.jsx
+++ src/components/navbar/NavbarStyled.jsx
@@ -7,9 +7,30 @@ export const NavbarStyled = styled.div`
     width: 100%;
     height: 60px;
     border: 1px solid black;
+    padding: 0 16px;
+
+    nav{
+        display: none; //${(props) => props.open ? 'flex' : 'none'};
+
+        ${({theme}) => theme.tablet`
+            display: flex;
+        `}
+
+        ul {
+            display: flex;
+            flex-direction: row;//modificar depois
+            text-align: center;
+            position: relative;
+            justify-content: flex-end;
+            list-style: none;
+            gap: 20px;
+        }
+    }
+
+
     
 `;
-export const MenuToggle = styled.div`
+export const MenuToggle = styled.button`
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -17,10 +38,17 @@ export const MenuToggle = styled.div`
     width: 40px;
     height: 40px;
 
+    border: 0;
+    background-color: transparent;
+
     & > div{ 
         height: 4px;
         width: 100%;
         background-color: black
         
     }
+
+    ${({theme}) => theme.tablet`
+        display: none;
+    `}
 `;
